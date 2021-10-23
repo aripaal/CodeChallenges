edca57de245d9860e04c870cdf7b93dae31c39d9
i350746 <arijit.pal@sap.com>
2021-10-23 14:06:46 +0530

    Initial commit

---
+++ src/main/java/SolutionTest.java
@@ -0,0 +1,45 @@
+import org.junit.*;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ronnie on 5/1/17.
+ */
+public class SolutionTest {
+
+    @Test
+    public void result() throws Exception {
+        ThreadMultiply multiply = new ThreadMultiply(1,2,3,4);
+        assertTrue(multiply.result()==21);
+    }
+    @Test
+    public void testLenghtOfLastWord(){
+        String word="";
+        assertTrue(Others.lengthOfLastWord(word)==0);
+        word="Helli I am Arijit";
+        assertTrue(Others.lengthOfLastWord(word)==6);
+
+    }
+    @Test
+    public void testLongestSubStringNoRepeat(){
+       assertTrue(Others.longestSubStringNoRepeat("abcdaadnaos").equals("abcd"));
+        //System.out.println(Others.longestSubStringNoRepeat("abcdaadna"));
+    }
+
+    @Test
+    public void testKthLargest(){
+        List<Integer> l = new LinkedList<>();
+        l.add(1);
+        l.add(4);
+        l.add(6);
+        l.add(3);
+
+
+        assertTrue(Others.findKLargest(l,2)==4);
+
+
+
+    }
+}
